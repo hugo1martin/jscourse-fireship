d6ea1867afdd88b2de78cef805f0d297fb47e018
Hugo Martin <hugomrt.94@gmail.com>
2022-12-08 22:06:47 +0100

    run --fix

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
-import './style.css'
+import './style.css';
 
 document.querySelector('form').addEventListener('submit', async (e) => {
-  showSpinner()
-  e.preventDefault()
+  showSpinner();
+  e.preventDefault();
 
-  const data = new FormData(e.target)
+  const data = new FormData(e.target);
 
   const response = await fetch('http://localhost:8080/dream', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      prompt: data.get('prompt'),
-    }),
-  })
+      prompt: data.get('prompt')
+    })
+  });
 
   if (response.ok) {
-    const { image } = await response.json()
+    const { image } = await response.json();
 
     const result = document.querySelector('#result');
-    result.innerHTML = `<img src="${image}" />`
-
+    result.innerHTML = `<img src="${image}" />`;
   } else {
-    const error = await response.text()
-    alert(error)
+    const error = await response.text();
+    alert(error);
   }
   hideSpinner();
 });
 
-function showSpinner() {
+function showSpinner () {
   const button = document.querySelector('button');
   button.disabled = true;
   button.innerHTML = 'Generating... <span class="spinner">🧠</span>';
 }
 
-function hideSpinner() {
+function hideSpinner () {
   const button = document.querySelector('button');
   button.disabled = false;
   button.innerHTML = 'Generate';
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 const openai = new OpenAIApi(configuration);
@@ -16,23 +16,23 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/dream', async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
+  try {
+    const prompt = req.body.prompt;
 
-        const aiResponse = await openai.createImage({
-            prompt,
-            n: 1,
-            size: '512x512',
-        })
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '512x512'
+    });
 
-        const image = aiResponse.data.data[0].url;
-        res.send({ image });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
-    }
+    const image = aiResponse.data.data[0].url;
+    res.send({ image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
 });
 
 app.listen(8080, () => {
-    console.log('Server listening on port 8080');
+  console.log('Server listening on port 8080');
 });
